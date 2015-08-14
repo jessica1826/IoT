204c1d88d1220ea1c2e3d3ef1ae854b2ca5e4757
jessica1826 <jessica.rodas02@gmail.com>
2015-08-14 15:03:09 -0500

    Modificación del código y respuesta de la preg 19
    Modificación del código y respuesta de la preg 19

--- ArduinoCparte1/Tomadedecisiones19/Tomadedecisiones19.ino
+++ ArduinoCparte1/Tomadedecisiones19/Tomadedecisiones19.ino
@@ -1,10 +1,10 @@
 void setup(){
   int k;
-  Serial.begin(9600);
+  Serial.begin(9600);//Inicialización puerto serial.
   
-  for (k=0,Serial.print("Expl1 k= "),Serial.println(k);
-      Serial.print("Exp2 k = "), Serial.println(k),k<10;
-      k++, Serial.print("Exp3 k = "), Serial.println(k)){
+  for (k=0,Serial.print("Expl1 k= "),Serial.println(k);//Expresion 1
+      Serial.print("Exp2 k = "), Serial.println(k),k<10;//Expresion 2
+      k++, Serial.print("Exp3 k = "), Serial.println(k)){//Expresion 3
         
         Serial.print("In loop body, k squared = ");
         Serial.println(k*k);
