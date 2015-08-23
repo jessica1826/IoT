8fbad68e8d656b950305a7144bc2ad06482a10f5
jessica1826 <jessica.rodas02@gmail.com>
2015-08-23 12:44:13 -0500

    Parte 1 terminada
    Parte 1 terminada

--- ArduinoCparte1/Tomadedecisiones20/Tomadedecisiones20.ino
+++ ArduinoCparte1/Tomadedecisiones20/Tomadedecisiones20.ino
@@ -1,22 +1,22 @@
 int searchList[200];
 
 void setup(){
-  int index;
-  int target = 5343;
-  Serial.begin(115200);
-  memset(searchList, 0, sizeof(searchList));
-  searchList[160] = target;
+  int index; // se declara una variable
+  int target = 5343;// se declara una variable y se le da un valor 
+  Serial.begin(115200); // Se inicializa el Arduino
+  memset(searchList, 0, sizeof(searchList)); //Rellena el bloque de memoria
+  searchList[160] = target; 
   
-  index=0;
-  while(true){
-    if(searchList[index] == target){
+  index=0; //Inicialización del contador
+  while(true){ //Iniciación del ciclo while
+    if(searchList[index] == target){ //Condicion que el valor de index se igual a target
       break;
     }
-    index++;
+    index++; //Contador 
   }
   
-  Serial.print("Found target at index= ");
-  Serial.println(index);
+  Serial.print("Found target at index= "); //Imprime en pantalla
+  Serial.println(index);//Imprime en pantalla el valor de index
 }
 
 void loop(){}
